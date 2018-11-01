1c23ac20ecf639e3f10ba05966bdea8adebdc379
curtisburns <curtis.burns28@gmail.com>
2018-11-01 10:20:36 +0000

    hooked up router and tested data retrieval

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const app = express();
-
+const { port, dbConfig } = require('./config/environment');
+const Router = require('./config/routes');
 // Database
-
 const mysql = require('mysql');
-
-
-const dbConfig = {
-  host: 'birdie-test.cyosireearno.eu-west-2.rds.amazonaws.com',
-  port: 3306,
-  user: 'test-read',
-  password: 'xnxPp6QfZbCYkY8',
-  name: 'birdietest',
-  table: 'census_learn_sql'
-};
 
 const con = mysql.createConnection(dbConfig);
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log('Connected!');
+  console.log('Now connected to birdietest database');
+  // con.query('SELECT * FROM census_learn_sql', function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
 });
 
+// connection.end();
 
-app.listen(4000, () => console.log('Express is listening on port 4000'));
+app.use('/api', Router);
+
+app.listen(port, () => console.log('Express is listening on port ' + port));
