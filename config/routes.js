bdcd011003e4aee759fff6770e7e76c99152c7e5
curtisburns <curtis.burns28@gmail.com>
2018-11-03 21:04:29 +0000

    removed dummy route

--- config/routes.js
+++ config/routes.js
@@ -2,11 +2,6 @@ const express = require('express');
 const Router = express.Router();
 const dataController = require('../controllers/dataController');
 
-Router.route('/')
-  .get((req, res)=> {
-    res.send('test');
-  });
-
 Router.route('/data')
   .get(dataController.gatherVariableNames);
 
